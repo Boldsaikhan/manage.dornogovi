903a2beeb296c233f2bb54464107a53300efbe82
Boldsaikhan <ca.boogii@gmail.com>
2026-08-20 11:00:44 +0800

    Богино замын setup.sh нэмэв (VNC консолд гараар бичихэд)

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Богино зам: deploy/server-setup.sh руу дамжуулна (консолд гараар бичихэд хялбар).
+exec bash -c "$(curl -fsSL https://raw.githubusercontent.com/Boldsaikhan/manage.dornogovi/main/deploy/server-setup.sh)"
